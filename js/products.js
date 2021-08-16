caaab0e7854a6b09f8a0a57985efa62f899cc4d2
Rosi <mrcabreracorrea@gmail.com>
2021-08-16 14:50:49 -0300

    Entrega 1

--- js/products.js
+++ js/products.js
@@ -10,12 +10,11 @@ function showCategoriesList(array){
         <div class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
-                    <img src="` + category.imgSrc + `" alt="` + category.desc + `" class="img-thumbnail">
+                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <h4 class="mb-1">`+ category.name +`</h4>
-                        <small class="text-muted">` + category.proCount + ` artículos</small>
                     </div>
                     <div>${category.description}</div>
                     <div>${category.currency} ${category.cost}</div>
