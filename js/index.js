1c423cb0a9038597e36a98bf4e4bffdd34eaa86a
Rosi <mrcabreracorrea@gmail.com>
2021-08-19 17:18:09 -0300

    Cambios

--- js/index.js
+++ js/index.js
@@ -6,5 +6,10 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    function signOut() {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+          console.log('User signed out.');
+        });
+      }
 });
